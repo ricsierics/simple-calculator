cf75ca4784fb7b6b571ea97180cb6d0784be1138
Enrico Magboo <enricom@magenic.com>
2018-07-31 19:50:42 +0800

    commit: done code behind, for submission

---
+++ SimpleCalculator/SimpleCalculator/Computer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    public class Computer
+    {
+        public double Compute(double number1, double number2, string operation)
+        {
+            switch (operation)
+            {
+                case "+": return (number1 + number2);
+                case "-": return (number1 - number2);
+                case "X": return (number1 * number2);
+                case "/":
+                    {
+                        if (number2 == 0)
+                            throw new DivideByZeroException();
+                        return (number1 / number2);
+                    }
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
--- SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
+++ SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
@@ -9,9 +9,63 @@ namespace SimpleCalculator
 {
 	public partial class MainPage : ContentPage
 	{
+        private double number1;
+        private double number2;
+        private string operation = string.Empty;
+        private int clickCounter = 0;
+        private bool isComputed = false;
+
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void OnClickedNumber(object sender, EventArgs e)
+        {
+            clickCounter++;
+
+            if (clickCounter < 2)
+            {
+                lblResult.Text = ((Button)sender).Text;
+                return;
+            }
+            lblResult.Text += ((Button)sender).Text;
+        }
+
+        private void OnClickedOperator(object sender, EventArgs e)
+        {
+            if (clickCounter > 0 || isComputed)
+            {
+                number1 = double.Parse(lblResult.Text);
+                operation = ((Button)sender).Text;
+                clickCounter = 0;
+            }
+        }
+
+        private void OnClickedEquals(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(operation))
+            {
+                number2 = double.Parse(lblResult.Text);
+                var computer = new Computer();
+                try
+                {
+                    lblResult.Text = computer.Compute(number1, number2, operation).ToString();
+                    clickCounter = 0;
+                    isComputed = true;
+                }
+                catch (Exception)
+                {
+                    lblResult.Text = "Math Error";
+                }
+            }
+        }
+
+        private void OnClickedClear(object sender, EventArgs e)
+        {
+            lblResult.Text = "0";
+            operation = string.Empty;
+            clickCounter = 0;
+        }
+    }
 }
